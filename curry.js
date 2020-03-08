3748316b4d568c4579c96bf7f579bd6c5cd284c5
juansalvatore <juansalvatore.ar@gmail.com>
2020-03-08 14:23:20 -0300

    Add rambda and more examples

--- curry.js
+++ curry.js
@@ -1,3 +1,5 @@
+const { curry } = require("ramda");
+
 // Isomorphic
 const toPair = f => ([x, y]) => f(x, y);
 const fromPair = f => (x, y) => f([x, y]);
@@ -6,7 +8,8 @@ const flip = f => (x, y) => f(y, z);
 const add = (a, b) => a + b;
 console.log(toPair(add)([1, 2]));
 
-const curry = f => x => y => f(x, y);
+// curry
+// const curry = f => x => y => f(x, y); // example for only two arguments
 const uncurry = f => (x, y) => f(x)(y);
 
 const curriedAdd = curry(add);
@@ -20,3 +23,9 @@ console.log(!!isOdd(1));
 const filter = curry((f, xs) => xs.filter(f));
 const getOdds = filter(isOdd);
 console.log(getOdds([1, 2, 3, 4, 5, 6]));
+
+const replace = curry((regex, replacement, str) =>
+  str.replace(regex, replacement)
+);
+const replaceVowels = replace(/[AEIOU]/gi, "!"); // point free (getting rid of arguments)
+console.log(replaceVowels("hello world"));
